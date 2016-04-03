768416917d53689cd2b0812a85600682e42d0bae
Chetan Jhaveri <cjhaveri@gmail.com>
2016-04-03 15:32:46 -0700

    Adding selection sort and test

---
+++ corejava/src/main/java/test/cjhaveri/corejava/SelectionSort.java
@@ -0,0 +1,57 @@
+package test.cjhaveri.corejava;
+
+import java.util.Arrays;
+
+public class SelectionSort extends SortBase {
+
+	public Integer [] selectionSort(Integer[] numbers) {
+		
+		//handle nulls
+		if (numbers == null || numbers.length == 0) {
+			return null;
+		}
+
+		int frontOfTheListIndex = 0;
+		//// when the "front" of the list is the size of the list you are done
+		for (int i = frontOfTheListIndex; i < numbers.length; i++) {
+			int currentLowest = numbers[frontOfTheListIndex];
+			int currentLowestIndex = frontOfTheListIndex;
+			// go through the rest of the list
+			for (int j = i; j < numbers.length; j++) {
+				numberOfOperations++;
+				if (j != numbers.length) {
+					int nextNumber = numbers[j];
+					// keep track of the lowest number at every pass
+					if (currentLowest > nextNumber) {
+						currentLowest = numbers[j];
+						currentLowestIndex = j;
+					}
+				}
+			} //end inner for loop
+
+			// put it at the "front" of the list
+			if (currentLowestIndex != frontOfTheListIndex) {
+				// now swap the front of the index with lowest
+				numbers[currentLowestIndex] = numbers[frontOfTheListIndex];
+				numbers[frontOfTheListIndex] = currentLowest;
+			}
+
+			//the "front" of the list is always increasing
+			frontOfTheListIndex++;
+
+			printArray(numbers);
+		}//end outer for loop
+		
+		return Arrays.copyOf(numbers, numbers.length);
+	}
+
+	public static void main(String[] args) {
+
+		SelectionSort ss = new SelectionSort();
+		Integer[] numbers = { 9, 4, 2, 0, -1, 8, 6, 2 };
+		ss.selectionSort(numbers);
+		ss.printNumberOfOperations();
+
+	}
+
+}
---
+++ corejava/src/test/java/test/cjhaveri/corejava/SelectionSortTest.java
@@ -0,0 +1,30 @@
+package test.cjhaveri.corejava;
+
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class SelectionSortTest extends SortTestBase {
+
+	@Test (invocationCount = 100)
+	public void testQuickSort() {
+		Integer[] numbers = createRandomNumberArrays();
+		//the test
+		SelectionSort ss = new SelectionSort();
+		Integer[] sortedNumbers = ss.selectionSort(numbers);
+		Arrays.sort(numbers);
+		Assert.assertEquals(Arrays.toString(sortedNumbers), Arrays.toString(numbers));
+	}
+
+	@Test
+	public void testQuickSortWithNullNumbers() {
+		Integer[] numbers = null;
+		//the test
+		SelectionSort ss = new SelectionSort();
+		Integer[] sortedNumbers = ss.selectionSort(numbers);
+		Assert.assertEquals(Arrays.toString(sortedNumbers), Arrays.toString(numbers));
+	}
+	
+}
