33c64e12bd2decb5abfbf3ce4935d23846f842e0
Chetan Jhaveri <cjhaveri@gmail.com>
2016-09-24 18:07:48 -0700

    Playing with Semaphores

--- src/main/java/test/cjhaveri/corejava/concurrency/concurrency/ExecutorTest.java
+++ src/main/java/test/cjhaveri/corejava/concurrency/concurrency/ExecutorTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 /**
  * Demo to just give all the java.util.Concurrent package a shot to explore the features
@@ -20,20 +21,22 @@ public class ExecutorTest implements Callable<States> {
     private int skipNumber;
     private int invocationCount;
 
+    private static Semaphore semaphore = new Semaphore(2, true);
+
     public static void main(String[] args) {
 
         int MAX_THREADS = 5;
 
         ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
 
-        List<String> states = Arrays.asList("CA", "NV", "DC", "IL", "WA");
+        List<String> states = Arrays.asList("CA", "NV", "DC", "IL", "WA", "NA", "NH", "AK");
 
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Submitting invocation: " + i);
             executorService.submit(new ExecutorTest(states, i));
         }
-//        executorService.shutdown();
+        executorService.shutdown();
     }
 
 
@@ -41,6 +44,7 @@ public class ExecutorTest implements Callable<States> {
         this.toProcess = states;
         this.skipNumber = skipNumber;
         this.invocationCount = skipNumber;
+        System.out.println("Available semaphores: " + semaphore.availablePermits());
     }
 
 
@@ -49,10 +53,17 @@ public class ExecutorTest implements Callable<States> {
 
         System.out.println("Processing thread id: " + Thread.currentThread().getId());
         States a = new States(toProcess, skipNumber);
+
+        System.out.println(Thread.currentThread().getId() + ": Acquring Sempahore") ;
+        semaphore.acquire();
+        System.out.println(Thread.currentThread().getId() + ": Acquired Semaphore, Available: " + semaphore.availablePermits() );
+
         Iterator i = a.iterator();
         while (i.hasNext()) {
             System.out.println(Thread.currentThread().getId() +":" + invocationCount +":"+i.next());
         }
+        semaphore.release();
+        System.out.println(Thread.currentThread().getId() + ": Releasing semaphore");
         return a;
 
     }
